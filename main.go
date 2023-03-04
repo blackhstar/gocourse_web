af20119c2a08f35200017b0a33b9240605f12a87
Hugo <hugopine87@gmail.com>
2023-03-04 12:15:56 -0600

    avance 2

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	port := ":3333"
-	http.HandleFunc("/users", getUsers)
-	http.HandleFunc("/courses", getCourses)
+	//http
+	//port := ":3333"
+	router := mux.NewRouter()
 
-	err := http.ListenAndServe(port, nil)
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/courses", getCourses).Methods("GET")
 
-	if err != nil {
-		fmt.Println(err)
+	srv := &http.Server{
+		Handler:      http.TimeoutHandler(router, time.Second*5, "Timeout!"),
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 3 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//http
+	//err := http.ListenAndServe(port, nil)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("get /Users")
+	//http
+	//fmt.Println("get /Users")
+	//io.WriteString(w, "This is my user endpint!\n")
 
-	io.WriteString(w, "This is my user endpint!\n")
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 
 }
 
 func getCourses(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("get /courses")
+	//http
+	//fmt.Println("get /courses")
+	//io.WriteString(w, "This is my course endpint!\n")
 
-	io.WriteString(w, "This is my course endpint!\n")
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 
 }
